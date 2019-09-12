4b56b57abe9b2878d9823823c7eb39fd81e2b283
Naresh <nareshp0024@gmail.com>
2019-09-11 22:49:08 -0400

    optimized lab_cycle_4/prog7_prime.c

--- lab_cycle_4/prog7_prime.c
+++ lab_cycle_4/prog7_prime.c
@@ -12,6 +12,12 @@ int main()
 		printf("Enter a number: ");
 		scanf("%d", &num);
 
+		if(num == 1) 
+		{	// special case
+			printf("1 is neither prime nor composite\n");
+			exit(1);
+		}
+
 		// logic
 		for(i = 2; i <= num/2; i++)		// its sufficient if divisibility is checked only till half of the number 
 		{
@@ -22,10 +28,7 @@ int main()
 			}
 		}
 
-		if(num == 1)	// special case
-			printf("1 is neither prime nor composite\n");
-		else
-			printf("%d is PRIME\n", num);
+		printf("%d is PRIME\n", num);
 
 	return 0;
 }
